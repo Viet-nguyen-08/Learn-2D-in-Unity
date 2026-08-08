500ac8df64ea277a2f53af664fa2c3b5771060bd
Viet-Vietnguyen-08 <nguyenhovivu@gmail.com>
2026-08-08 22:59:06 +0700

    AI Enemy

--- Assets/Folder/enemy/enemy2.cs
+++ Assets/Folder/enemy/enemy2.cs
@@ -4,25 +4,44 @@ using UnityEngine;
 
 public class enemy2 : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 200f;
-    private float ran, ran2;
-
-    private Rigidbody2D rb;
-
+    Vector2 pointA;
+    Vector2 pointB;
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        pointA = transform.position;
+        pointB = new Vector2(Random.Range(-12f, 12f), Random.Range(-12f, 12f));
     }
-
+    float elapsedTime = 0f;
+    float duration = 3f;
     void Update()
-    {        
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            transform.position = new Vector2(ran, ran2);
-            ran = Random.Range(-6, 6);
-            ran2 = Random.Range(-6, 6);
-        }
+    {
+        elapsedTime += Time.deltaTime;
+        float t = elapsedTime / duration;
+        transform.position = Vector2.Lerp(pointA, pointB, t);
     }
     
 }
+/*  
+public float moveTime = 2f;
+    private Vector2 startPoint;
+    private Vector2 targetPoint;
+    private float timer;
+    void Start()
+    {
+        NewTarget();
+    }
+    void Update()
+    {
+        timer -= Time.deltaTime;
+        float t = timer / moveTime;
+        transform.position = Vector2.Lerp(startPoint, targetPoint, t);
+        if(t >= 1f)NewTarget();
+    }    
+    void NewTarget()
+    {
+        startPoint = transform.position;
+        targetPoint = new Vector2(Random.Range(-12f, 12f), Random.Range(-12f, 12f));
+        timer = 0f;
+    }
+
+*/
